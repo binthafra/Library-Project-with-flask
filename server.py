664d74ed43c15cfdf6e5175d7926d128b12a83a1
HP <binthafra@gmail.com>
2022-02-10 22:40:54 +0600

    Create simple library management app using Flask API

---
+++ server.py
@@ -0,0 +1,57 @@
+import psycopg2
+import os
+from flask import Flask, render_template, url_for
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="flask_db",
+    user='postgres',
+    password='afra1234')
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
+# Execute a command: this creates a new table
+cur.execute('DROP TABLE IF EXISTS books;')
+cur.execute('CREATE TABLE books (id serial PRIMARY KEY,'
+            'title varchar (150) NOT NULL,'
+            'author varchar (50) NOT NULL,'
+            'pages_num integer NOT NULL,'
+            'review text,'
+            'date_added date DEFAULT CURRENT_TIMESTAMP);'
+            )
+
+# Insert data into the table
+
+cur.execute('INSERT INTO books (title, author, pages_num, review)'
+            'VALUES (%s, %s, %s, %s)',
+            ('A Tale of Two Cities',
+             'Charles Dickens',
+             489,
+             'A great classic!')
+            )
+
+
+cur.execute('INSERT INTO books (title, author, pages_num, review)'
+            'VALUES (%s, %s, %s, %s)',
+            ('Anna Karenina',
+             'Leo Tolstoy',
+             864,
+             'Another great classic!')
+            )
+
+conn.commit()
+
+cur.close()
+conn.close()
+
+
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
+
+
+# @app.route("/base")
+# def base():
+#     # return render_template('./base.html')
+#     return "<p>Hiii, World!</p>"
